fb9b392f424104b6557bb7be9a41b9b13077a6e2
Антон Чирков <lordtrimbo@mail.ru>
2022-09-24 20:00:50 +0400

    Добавьте файлы проекта.

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PickACardUI
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            int suit = random.Next(1, 5);
+            if (suit == 1) return "Spades";
+            if (suit == 2) return "Hearts";
+            if (suit == 3) return "Clubs";
+            return "Diamonds";
+        }
+    }
+}
